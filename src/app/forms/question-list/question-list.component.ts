ac791c18e27ea400c31721a5031697bdeb0dbd9f
aline-aline <alinecris@gmail.com>
2017-12-17 21:55:13 -0200

    Add question form and question list components

--- src/app/forms/question-list/question-list.component.ts
+++ src/app/forms/question-list/question-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { QuestionService } from '../../shared/question.service';
+import { Question } from '../../shared/question.model';
 
 @Component({
   selector: 'app-question-list',
@@ -6,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./question-list.component.css']
 })
 export class QuestionListComponent implements OnInit {
+
+  @Input() questions: Question[];
+  @Input() form_id: number;
+  public question_new: Question = new Question({});
 
   constructor() { }
 
