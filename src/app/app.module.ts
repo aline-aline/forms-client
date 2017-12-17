46f55732365a850b33daa1dfca63cb9b2e019ace
aline-aline <alinecris@gmail.com>
2017-12-17 10:23:20 -0200

    Add materialize, color picker and graph charts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MaterializeModule } from 'ng2-materialize';
+import { ColorPickerModule } from 'ngx-color-picker';
+import { ChartsModule } from 'ng2-charts';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterializeModule.forRoot(),
+    ColorPickerModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
